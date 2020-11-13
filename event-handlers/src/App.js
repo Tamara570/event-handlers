b30ca3e766adfbb9aa4fb74e2a760a92793c4e2a
Tamara Walker <tamaralw115@gmail.com>
2020-11-13 16:16:03 -0500

    first commit, unfinished work

---
+++ event-handlers/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Accordion from './Accordion'
+import './App.css';
+
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+          <p>
+           <Accordion sections={sections}/>
+          </p>
+      </div>
+    );
+  }
+}
+
+export default App;
